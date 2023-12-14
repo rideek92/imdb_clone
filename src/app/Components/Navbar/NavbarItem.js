9fd4f7562cfc608bb4c2708bb47f59e223cfbcce
Rideek <rabinskid@rabdev.pl>
2023-12-14 12:17:22 +0100

    add useSearchParams to navigation

--- src/app/Components/Navbar/NavbarItem.js
+++ src/app/Components/Navbar/NavbarItem.js
@@ -1,10 +1,18 @@
 import Link from "next/link";
 
+import {useSearchParams} from "next/navigation";
+
 
 export default function NavbarItem({title, param}){
 
+    const searchParams = useSearchParams()
+    const genre = searchParams.get('genre')
+
     return(
-        <Link className="mx-3 uppercase tracking-wider select-none hover:text-amber-600" href={`/?=${param}`}>
+        <Link className={`mx-3 uppercase tracking-wider select-none ${
+            genre && genre === param && `underline underline-offset-8 decoration-4 decoration-gray-700 dark:decoration-amber-400 
+            rounded-lg `
+        }`} href={`/?genre=${param}`}>
             {title}
         </Link>
     )
--- src/app/about/page.js
+++ src/app/about/page.js
@@ -4,8 +4,8 @@ export default function About(){
 
     return(
         <div className="mx-2">
-            <h1 className="my-3 text-2xl text-amber-600">About</h1>
-            <p className="tracking-[1px]">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci autem cumque dicta dolorem impedit iusto laudantium molestias nulla obcaecati omnis, quaerat, quos reiciendis veniam? Accusamus animi asperiores at blanditiis cum ducimus et eum eveniet explicabo illo impedit, incidunt ipsa minima, minus, necessitatibus nesciunt obcaecati officiis placeat praesentium provident quae qui quo rem repellendus tenetur velit voluptatum. Consequatur laborum repellendus totam velit. Blanditiis dolore et in, laudantium magnam magni officiis perspiciatis possimus provident quae similique vero, voluptas. Animi aspernatur delectus fugit id iste odio quibusdam recusandae similique vitae voluptatibus. Autem consequuntur cumque deserunt in minus modi odit provident quae quidem vitae!</p>
+            <h1 className="my-3 text-2xl text-amber-600 ">About</h1>
+            <p className="tracking-[1px] max-sm:text-[1.2rem] ">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci autem cumque dicta dolorem impedit iusto laudantium molestias nulla obcaecati omnis, quaerat, quos reiciendis veniam? Accusamus animi asperiores at blanditiis cum ducimus et eum eveniet explicabo illo impedit, incidunt ipsa minima, minus, necessitatibus nesciunt obcaecati officiis placeat praesentium provident quae qui quo rem repellendus tenetur velit voluptatum. Consequatur laborum repellendus totam velit. Blanditiis dolore et in, laudantium magnam magni officiis perspiciatis possimus provident quae similique vero, voluptas. Animi aspernatur delectus fugit id iste odio quibusdam recusandae similique vitae voluptatibus. Autem consequuntur cumque deserunt in minus modi odit provident quae quidem vitae!</p>
         </div>
 
     )
